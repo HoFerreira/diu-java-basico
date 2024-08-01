10d86211afee705d315719ada113886a663ccd64
Hosana <hosanaferreira19@gmail.com>
2024-07-31 21:32:44 -0300

    Estruturas Condicionais

---
+++ Estruturas Condicionais/Condicionais/Simples/src/Encadeadas.java
@@ -0,0 +1,18 @@
+public class Encadeadas {
+        public static void main(String[] args) {
+            int nota = 5;
+    
+            if(nota >= 7)
+                System.out.println("Aprovado");
+            
+            else if (nota > 5 && nota < 7)
+                System.out.println("Recuperaçao");
+
+            else
+                System.out.println("Reprovado");
+    
+                System.out.println(nota);
+        }
+    }
+    
+
